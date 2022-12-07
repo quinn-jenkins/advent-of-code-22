d8b7384ea7f6d6897d90ac147f9ed4a18bb8beca
quinn <quinn3spam@gmail.com>
2022-12-06 22:50:49 -0800

    added day 3, written in GO

--- adventofcode/day2/__init__.py
+++ adventofcode/day2/__init__.py
@@ -1,3 +1,9 @@
+def get_opp_choice_value(opponentChoice):
+    return ord(opponentChoice) - 64
+
+def get_self_choice_value(selfChoice):
+    return ord(selfChoice) - 87
+
 def is_win(opponentChoice, selfChoice):
     if opponentChoice == "A" and selfChoice == "Y":
         return True
@@ -9,11 +15,11 @@ def is_win(opponentChoice, selfChoice):
     return False
 
 def is_draw(opponentChoice, selfChoice):
-    if (ord(opponentChoice) - 64) == (ord(selfChoice) - 87):
+    if get_opp_choice_value(opponentChoice) == get_self_choice_value(selfChoice):
         return True
     return False
 
-if __name__ == '__main__':
+def part_one():
     totalScore = 0
     with open("input.txt", 'r') as file:
         for line in file:
@@ -21,14 +27,56 @@ if __name__ == '__main__':
             choices = line.split(" ")
 
             if is_win(choices[0], choices[1]):
-                totalScore += ((ord(choices[1][0]) - 87) + 6)
+                totalScore += (get_self_choice_value(choices[1]) + 6)
                 print(f"Win -- score is now {totalScore}")
             elif is_draw(choices[0], choices[1]):
-                totalScore += (ord(choices[0][0]) - 64 + 3)
+                totalScore += (get_opp_choice_value(choices[0]) + 3)
                 print(f"Tie -- score is now {totalScore}")
             else:
                 totalScore += (ord(choices[1][0]) - 87)
                 print(f"Loss -- score is now {totalScore}")
+
+def get_losing_score(opponentChoice):
+    if opponentChoice == "A":
+        return 3
+    elif opponentChoice == "B":
+        return 1
+    else:
+        return 2
+
+def get_winning_score(opponentChoice):
+    if opponentChoice == "A":
+        return 2
+    elif opponentChoice == "B":
+        return 3
+    else:
+        return 1
+
+def part_two():
+    totalScore = 0
+    with open("input.txt", 'r') as file:
+        for line in file:
+            line = line.strip()
+            choices = line.split(" ")
+
+            if choices[1] == "X":
+                # lose
+                totalScore += get_losing_score(choices[0])
+                print(f"Lose -- score is now {totalScore}")
+            elif choices[1] == "Y":
+                # tie
+                totalScore += get_opp_choice_value(choices[0]) + 3
+                print(f"Tie -- score is now {totalScore}")
+            elif choices[1] == "Z":
+                # win
+                totalScore += get_winning_score(choices[0]) + 6
+                print(f"Win -- score is now {totalScore}")
+
+
+
+if __name__ == '__main__':
+    # part_one()
+    part_two()
 
 
 
---
+++ go/day3.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"unicode"
+)
+
+func main() {
+	//partOne()
+	partTwo()
+}
+
+func partTwo() {
+	// open a file
+	f, err := os.Open("day3Input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	total := 0
+	lines := []string{}
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+
+		if len(lines) == 3 {
+			// process block of 3 lines
+			val := processGroup(lines)
+			total += val
+			// reset lines
+			lines = []string{}
+		}
+	}
+
+	fmt.Println("TOTAL:", total)
+}
+
+func processGroup(strings []string) int {
+	intersection := HashGeneric([]rune(strings[0]), []rune(strings[1]))
+	intersection = HashGeneric(intersection, []rune(strings[2]))
+	if len(intersection) < 1 {
+		fmt.Println("ERROR!!! No Intersection")
+	}
+
+	//for i := 0; i < len(intersection); i++ {
+	//	fmt.Println("Repeat", string(intersection[i]))
+	//}
+	return ValueForChar(intersection[0])
+}
+
+func partOne() {
+	// open a file
+	f, err := os.Open("day3Input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	total := 0
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		val := processLine(scanner.Text())
+		total += val
+		fmt.Println("New Total", total)
+	}
+
+	fmt.Println("TOTAL:", total)
+}
+
+func processLine(text string) int {
+	length := len(text)
+	halfLength := length / 2
+
+	fmt.Println("Full String", text)
+	str1 := text[0:halfLength]
+	//fmt.Println("\tFirstHalf ", str1, "Length", len(str1))
+	str2 := text[halfLength:length]
+	//fmt.Println("\tSecondHalf", str2, "Length", len(str2))
+
+	intersection := HashGeneric([]rune(str1), []rune(str2))
+	if len(intersection) < 1 {
+		fmt.Println("ERROR!!!")
+	}
+
+	repeat := intersection[0]
+	val := ValueForChar(repeat)
+	fmt.Println("Repeat Char", string(repeat), "Value", val)
+
+	return val
+
+}
+
+func ValueForChar(ch rune) int {
+	if unicode.IsUpper(ch) {
+		return int(ch) - 38
+	} else {
+		return int(ch) - 96
+	}
+}
+
+// Hash has complexity: O(n * x) where x is a factor of hash function efficiency (between 1 and 2)
+func HashGeneric[T comparable](a []T, b []T) []T {
+	set := make([]T, 0)
+	hash := make(map[T]struct{})
+
+	for _, v := range a {
+		hash[v] = struct{}{}
+	}
+
+	for _, v := range b {
+		if _, ok := hash[v]; ok {
+			set = append(set, v)
+		}
+	}
+
+	return set
+}
